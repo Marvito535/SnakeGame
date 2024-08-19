030ebb82b66e05e19d8e2210ea95dcf9f135f62a
Marvito535 <marvin.bahde90@gmail.com>
2024-08-19 14:08:16 -0300

    basic Snake class modified

--- js/snake.js
+++ js/snake.js
@@ -1,21 +1,70 @@
 class Snake {
     constructor() {
-        this.body = [{ x: 10, y: 10 }];  
-        this.direction = { x: 1, y: 0 };  
+        this.segment = [
+            { x: 10, y: 10 },
+            { x: 10, y: 11 },
+            { x: 10, y: 12 }
+        ];  
+        this.direction = 'UP'; // Initial direction
+        this.growing = false;
     }
-
-    update() {                                      // bewegt die Schlange vorwärts
-        const head = {
-            x: this.body[0].x + this.direction.x,  // this.body gibt das erste Segment der Schlange zurück, das als Kopf fungiert. this.direction gibt die aktuelle Bewegungsrichtung der Schlange an
-            y: this.body[0].y + this.direction.y,  
+                                                        
+       setDirection(newDirection)  {       // Set the direction of the snake
+        const oppositeDirections = {
+            'UP' : 'Down',
+            'DOWN' : 'UP',
+            'LEFT' : 'RIGHT',
+            'RIGHT' : 'LEFT'
         };
-        this.body.unshift(head);                    // unshift() fügt ein neues Element am Anfang eines Arrays hinzu.
-        this.body.pop();                            //pop() entfernt das letzte Element eines Arrays.
+
+            if(newDirection !== oppositeDirections[this.direction]) {    // Prevent the snake from reversing
+                this.direction = newDirection;
+            }
+       }                                               
+   
+                                                                     
+        move() {                                    // Move the snake forward                                              
+        const head = { ...this.segment[0] };        // copy of the array
+        switch (this.direction) {                   // Determine the new head position based on the direction
+            case 'UP':
+                head.y -= 1;
+                break;
+            case 'DOWN':
+                head.y += 1;
+                break;
+            case 'LEFT':
+                head.x -= 1;
+                break;
+            case 'RIGHT':
+                head.x += 1;
+                break; 
+        }
+           
+        
+        this.segment.unshift(head);                       // Add the new element to the front of the array
+            if (!this.growing) {
+            this.segment.pop();                          //pop() deletes the last element of the arrays.
+        }                                      
+            this.growing = false;                       // Reset growing status after moving
+    };
+        checkCollision() {
+        const head = this.segment[0];
+        for (let i = 1; i < this.segment.length; i++) {                               // Check if the head collides with any other segment
+            if (this.segment[i].x === head.x && this.segment[i].y === head.y) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    draw(ctx) {
+    grow() {                                            // Grow the snake 
+        this.growing = true;
+    }
+
+        draw(ctx) {
         ctx.fillStyle = 'lime';
-        this.body.forEach(segment => {             //callback ´function
+        this.body.forEach(segment => {             //callback function
             ctx.fillRect(segment.x * 20, segment.y * 20, 20, 20); // 20x20 pix at position 20,20
         });
     }
